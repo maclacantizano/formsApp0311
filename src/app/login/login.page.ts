279d1b37e4895b063266d26ef988ae2fc2707567
Maria Clara Macedo Cantizano <maria.cantizano@etec.sp.gov.br>
2022-11-17 15:03:17 -0300

    validação
    foi inserido a validação no formsApp

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formLogin = this.formBuilder.group({email: ['']});
+
+
+  constructor( private formBuilder: FormBuilder) { }
+
 
   ngOnInit() {
   }
