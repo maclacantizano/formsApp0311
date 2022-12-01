b7dcba92446572b9f92e28a6f4c57cb16803fe40
maclacantizano <maria.cantizano@etec.sp.gov.br>
2022-12-01 12:15:43 -0300

    validação
    registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formRegistro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formRegistro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['', Validators.compose([Validators.required])],
+      email:['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+      confirmarSenha: ['',  Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+
+    });
+   }
 
   ngOnInit() {
+  }
+
+  salvarRegistro(){
+    console.log('Formulário: ', this.formRegistro.valid);
+
   }
 
 }
