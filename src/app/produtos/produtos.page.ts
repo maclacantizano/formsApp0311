7a994d8389e41fafed00b661ab5dbb4ad074f21b
maclacantizano <maria.cantizano@etec.sp.gov.br>
2022-12-07 09:52:54 -0300

    cadastro de produtos
    falta salvar

--- src/app/produtos/produtos.page.ts
+++ src/app/produtos/produtos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-produtos',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosPage implements OnInit {
 
-  constructor() { }
+  formProdutos: FormGroup;
+
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo nome é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O campo deve ter pelo menos três caracteres.' }
+    ],
+    descricao: [
+      { tipo: 'required', mensagem: 'O campo descrição é obrigatório.' }
+    ],
+    validade: [
+      { tipo: 'required', mensagem: 'O campo validade é obrigatório.' }
+    ],
+    preco: [
+      { tipo: 'required', mensagem: 'O campo preço é obrigatório.' }
+    ],
+  };
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formProdutos = formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      descricao: ['', Validators.compose([Validators.required])],
+      validade: ['', Validators.compose([Validators.required])],
+      preco: ['', Validators.compose([Validators.required])]
+    });
+   }
 
   ngOnInit() {
+  }
+
+  salvarProdutos() {
+    console.log('Formulário: ', this.formProdutos.valid);
   }
 
 }
